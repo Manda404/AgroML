914a4a5bcafff10b016208cac631a243ce626271
Manda Surel <125080872+Manda404@users.noreply.github.com>
2025-03-10 21:55:55 +0100

    update

--- train/train_model.py
+++ train/train_model.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import joblib  # Pour sauvegarder le modèle
-from src.preprocessing import preprocess_data
+from preprocessing import preprocess_data
 
 
 def split_dataset(df):
