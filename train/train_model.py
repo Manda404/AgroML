c60f0019a0e94a498987fecf0dd41faa5a0aca4c
Manda Surel <125080872+Manda404@users.noreply.github.com>
2025-03-11 05:08:10 +0100

    Update news code  train

--- train/train_model.py
+++ train/train_model.py
@@ -4,6 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import joblib  # Pour sauvegarder le modèle
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from preprocessing import preprocess_data
 
 
